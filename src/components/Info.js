6b472e443bbe3b1f82e3a605194f99742b53ff2f
Timothy <102215468+timzw961@users.noreply.github.com>
2022-04-12 13:01:06 +1000

    test

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,24 @@
 import react from "react"
+import tim from "../images/timWang.jpg"
+import mail from "../images/mailicon.jpg"
+import linkin from "../images/linkedin.png"
 
 export default function Info(){
     return (
         <info>
-            <h1>This is the info section</h1>
+            <img className= "info_photo" src={tim}/>
+            <h1 className = "info_name"> Timothy Wang </h1>
+            <h2 className = "info_position"> Frontend Developer </h2>
+            <h4 className = "info_website"> timothywang.website </h4>
+            <div className ="info_button_container">
+                <button className = "info_emailbutton" > 
+                    <img src={mail}/>
+                    Email 
+                </button>
+                <button className = "info_linkedinbutton"> 
+                    <img src={linkin}/> LinkedIn 
+                </button>
+            </div>
         </info>
     )
 }
