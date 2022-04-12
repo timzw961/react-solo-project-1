6f13e8af91acb5c23df2c3d8ac409967466c965e
Timothy <102215468+timzw961@users.noreply.github.com>
2022-04-12 16:27:08 +1000

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer(){
     return (
         <div className = "footer_body">
             <div className = "footer_container">
-                <img className = "footer_twit" src="../images/Twitter_Icon.png" />
-                <img className = "footer_fb" src="../images/Facebook_Icon.png" />
-                <img className = "footer_insta" src="../images/Instagram_Icon.png" />
-                <img className = "footer_git" src="../images/GitHub_Icon.png" />
+                <img className = "footer_twit" src="../images/Twitter Icon.png" />
+                <img className = "footer_fb" src="../images/Facebook Icon.png" />
+                <img className = "footer_insta" src="../images/InstagramIcon.png" />
+                <img className = "footer_git" src="../images/GitHub Icon.png" />
              </div>
         </div>
     )
